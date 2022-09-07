386c394d076f4b87bbddb54cd955b2b7b2dd5aa6
Jonathan Felix <jhow.wi56@gmail.com>
2022-09-06 21:45:36 -0300

    findAll

--- src/main/java/br/com/fiap/epictaskapi/controller/UsuarioController.java
+++ src/main/java/br/com/fiap/epictaskapi/controller/UsuarioController.java
@@ -1,12 +1,25 @@
 package br.com.fiap.epictaskapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.epictaskapi.model.User;
+import br.com.fiap.epictaskapi.service.UserService;
 
 @RestController
 @RequestMapping("/api/user")
 public class UsuarioController {
 	
+	@Autowired
+	private UserService service;
 	
+	
+	public Page<User> index(Pageable pageable){
+		return service.listAll(pageable);
+		
+	}
 
 }
